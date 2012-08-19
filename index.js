de5bd96959cbf766f5a52048346d7e3d936f8a14
Israel Gutiérrez <gootyfer@gmail.com>
2012-08-19 16:50:45 +0200

    Fix path.exists and query tiny arm on server run

--- index.js
+++ index.js
@@ -72,7 +72,7 @@ var serve_http = function(request, response){
 			
     }
     
-    fs.exists(filePath, function(exists) {
+    path.exists(filePath, function(exists) {
     	if (exists) {
             fs.readFile(filePath, function(error, content) {
                 if (error) {
@@ -259,6 +259,7 @@ app.get('/risk', function (request, response) {
 });
 //Check every 10 minutes
 setInterval(queryTinyArm, 10*60*1000);
+setTimeout(queryTinyArm, 2000);
 //Launch app
 app.listen(80);
 console.log('Server running...');
